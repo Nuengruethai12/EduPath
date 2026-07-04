ae683617a4f40f27c3556b0a1058cb1191672810
Nuengruethai12 <kaewphvksh@gmail.com>
2026-07-04 13:15:18 +0700

    Fix apiKey and add measurementId in firebase config

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCY3moinLt5RScjsZA2Pd3z9KwRbi7e_M",
+  apiKey: "AIzaSyCCY3moinLt5RScjsZAzPd3z9KwRbi7e_M",
   authDomain: "edupath-e311c.firebaseapp.com",
   projectId: "edupath-e311c",
   storageBucket: "edupath-e311c.firebasestorage.app",
   messagingSenderId: "11042618134",
-  appId: "1:11042618134:web:e90c9184fc227eb0812e37"
+  appId: "1:11042618134:web:e90c9184fc227eb0812e37",
+  measurementId: "G-EXJ8HGT4ZT"
 };
 
 const app = initializeApp(firebaseConfig);
