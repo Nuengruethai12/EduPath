524cdcb1df201e614ffca1b3ad176016372a066b
Nuengruethai12 <kaewphvksh@gmail.com>
2026-07-04 11:32:28 +0700

    Add firebase.js for Firebase initialization

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCY3moinLt5RScjsZA2Pd3z9KwRbi7e_M",
+  authDomain: "edupath-e311c.firebaseapp.com",
+  projectId: "edupath-e311c",
+  storageBucket: "edupath-e311c.firebasestorage.app",
+  messagingSenderId: "11042618134",
+  appId: "1:11042618134:web:e90c9184fc227eb0812e37"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
